ddcd8bdaa060086f4f1874e8ee3431fafb99b9b6
anginputih-droid <angin.putih@ui.ac.id>
2026-09-14 13:28:42 +0700

    Assignment 2: Connecting the project's View to the project's Template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Project
 
 def show_main(request):
     context = {
@@ -21,3 +21,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_project(request):
+    context = {
+        "name": "Angin Putih Ranulaksmi Tsurayya Hapsoro",
+        "project_list": Project.objects.all(),
+    }
+    return render(request, "project.html", context)
