defdecddf257258145c4808569f652b1214679b1
anginputih-droid <angin.putih@ui.ac.id>
2026-09-21 18:51:05 +0700

    Assignment 3: Implementing the Form and Adapting the Page for Experience section

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from main.models import Project
-from django.forms import TextInput, Textarea, URLInput
+from django.forms import TextInput, Textarea, URLInput, Select
 
 class ProjectForm(forms.ModelForm):
     class Meta:
@@ -42,6 +42,50 @@ class ProjectForm(forms.ModelForm):
             "project_url": URLInput(
                 attrs={
                     "placeholder": "https://github.com/anginputih-droid",
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+        }
+
+
+# Add Experience Form
+class ExperienceForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "category",
+            "thumbnail",
+        ]
+
+        labels = {
+            "title": "Experience Title",
+            "description": "Experience Description",
+            "category": "Experience Category",
+            "thumbnail": "URL Picture",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Tell us about your experience",
+                    "rows": 3,
+                }
+            ),
+            "category": Select(
+                attrs={
+                    "placeholder": "Category",
                 }
             ),
             "thumbnail": URLInput(
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from main.views import (show_main, show_experience, show_projects, 
-                        create_project, get_projects_json, delete_project)
+from main.views import (show_main, show_experience, show_projects, create_project, 
+                        get_projects_json, delete_project, create_experience)
 
 app_name = "main"
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
-    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project")
+    path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+    path("experience/add/", create_experience, name="create_experience"),
 ]
